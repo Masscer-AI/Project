029cb88eccc798815a6b7fc92101d01093908ef2
Charlytoc <charlyjchaconc@gmail.com>
2024-10-18 14:57:37 -0500

    modify init.sh command to fetch latest changes

--- init.sh
+++ init.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-# Default value for the Django flag
+# Default values for the flags
 DJANGO=true
+INSTALL=false
 
 # Parse command line arguments
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         -d|--django) DJANGO="$2"; shift ;;
+        -i|--install) INSTALL=true ;;  # New flag for installation
         *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
 
 source venv/Scripts/activate
+
+# Execute installation commands if the flag is true
+if [ "$INSTALL" = true ]; then
+    git pull
+    pip install -r requirements.txt
+    cd ./streaming
+    npm i
+    cd ..  # Go back to the previous directory
+fi
 
 # Run Django application if the flag is true
 if [ "$DJANGO" = true ]; then
