d9467a129cc95e62b8a80292a6076492f09d5049
Charlytoc <charlyjchaconc@gmail.com>
2024-10-18 17:15:24 -0500

    fix error if the database is not ready yet

--- api/ai_layers/apps.py
+++ api/ai_layers/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
 from api.utils.color_printer import printer
+from django.db.utils import OperationalError
 
 class AiLayersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
@@ -15,6 +16,9 @@ class AiLayersConfig(AppConfig):
         # from api.providers.models import AIProvider
         # from api.utils.ollama_functions import list_ollama_models
         # from api.utils.openai_functions import list_openai_models
-
-        printer.blue(f"Running startup function for {self.name}")
-        check_models_for_providers()
+        try:
+            printer.blue(f"Running startup function for {self.name}")
+            check_models_for_providers()
+        except OperationalError:
+            # This exception might occur during migrations or if the database is not ready
+            printer.red("Database is not ready. Skipping AIProvider check.")
